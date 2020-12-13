95a6047cdffa407df1f03b0518b23183e79649e3
dcclifford <danielcannonclifford@gmail.com>
2020-12-13 00:42:36 -0500

    last

--- pet-clinic-web/src/test/java/guru/springframework/springpetclinic/SpringPetclinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/springpetclinic/SpringPetclinicApplicationTests.java
@@ -1,7 +1,6 @@
 package guru.springframework.springpetclinic;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
