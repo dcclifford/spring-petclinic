46456b686b34dfa2e1b6d026790811ed6847c960
dcclifford <danielcannonclifford@gmail.com>
2020-12-12 20:25:28 -0500

    working builds in intellij

--- pet-clinic-web/src/main/java/guru/springframework/springpetclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/springpetclinic/PetClinicApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author Dave Syer
  */
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
--- pet-clinic-web/src/test/java/guru/springframework/springpetclinic/SpringPetclinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/springpetclinic/SpringPetclinicApplicationTests.java
@@ -2,12 +2,17 @@ package guru.springframework.springpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 
 @SpringBootTest
-class SpringPetclinicApplicationTests {
+class SpringPetClinicApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
